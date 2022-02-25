fb69012fe335027d36223417db377fd97824744b
yuxiongfeng <1040217735@qq.com>
2022-02-25 15:26:06 +0800

    - 新增 心电贴蓝牙sdk demo

--- app/src/main/java/com/proton/patch/ble/MainActivity.java
+++ app/src/main/java/com/proton/patch/ble/MainActivity.java
@@ -9,6 +9,8 @@ import com.proton.ecgpatch.connector.EcgPatchManager;
 import com.proton.ecgpatch.connector.callback.DataListener;
 import com.wms.ble.bean.ScanResult;
 import com.wms.ble.callback.OnConnectListener;
+import com.wms.ble.callback.OnScanListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EcgPatchManager.init(this);
         //需要获取定位权限，否则搜索不到设备
         PermissionUtils.getLocationPermission(this);
 
-        findViewById(android.R.id.content).setOnClickListener(v -> {
+        findViewById(R.id.idText).setOnClickListener(v -> {
             testPatchConnect();
         });
     }
 
+    private void testScanPatch() {
+        EcgPatchManager.scanDevice(new OnScanListener() {
+            @Override
+            public void onScanStart() {
+                super.onScanStart();
+            }
+
+            @Override
+            public void onDeviceFound(ScanResult scanResult) {
+                super.onDeviceFound(scanResult);
+                Log.w(TAG, "扫描的设备:" + scanResult.getMacaddress());
+            }
+
+            @Override
+            public void onScanStopped() {
+                super.onScanStopped();
+            }
+
+            @Override
+            public void onScanCanceled() {
+                super.onScanCanceled();
+            }
+        });
+    }
+
     private void testPatchConnect() {
-        EcgPatchManager.init(this);
-        EcgPatchManager ecgPatchManager = EcgPatchManager.getInstance("C4:A1:1B:19:BB:C7");
+
+        EcgPatchManager ecgPatchManager = EcgPatchManager.getInstance("C9:57:0B:99:0D:23");
         ecgPatchManager.setDataListener(new DataListener() {
             @Override
             public void receiveEcgRawData(byte[] bytes) {
@@ -46,20 +73,24 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "是否跌倒: " + isFallDown);
             }
         });
+
         ecgPatchManager.connectEcgPatch(new OnConnectListener() {
             @Override
-            public void onConnectSuccess() {
+            public void onConnectSuccess(boolean isNewUUID) {
+                super.onConnectSuccess(isNewUUID);
                 Log.e(TAG, "连接成功");
             }
 
             @Override
             public void onConnectFaild() {
+                super.onConnectFaild();
                 Log.e(TAG, "连接失败");
             }
 
             @Override
-            public void onDisconnect(boolean b) {
-                Log.e(TAG, "断开连接，是否手动断开:" + b);
+            public void onDisconnect(boolean isManual) {
+                super.onDisconnect(isManual);
+                Log.e(TAG, "断开连接，是否手动断开:" + isManual);
             }
         });
     }
